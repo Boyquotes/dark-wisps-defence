826a38e46de0ccc270b2ddf1d2d7f1267d97d206
Daniel <arrekin@arrekin.com>
2023-10-26 08:58:22 +0200

    Flying effect for cannonballs

--- src/projectiles/cannonball.rs
+++ src/projectiles/cannonball.rs
@@ -1,3 +1,4 @@
+use std::f32::consts::PI;
 use bevy::math::Vec3Swizzles;
 use bevy::prelude::*;
 use crate::common_components::{Health};
@@ -12,7 +13,10 @@ pub struct MarkerCannonball;
 
 // Cannonball follows Wisp, and if the wisp no longer exists, follows the target vector
 #[derive(Component, Default)]
-pub struct CannonballTarget(Vec2);
+pub struct CannonballTarget{
+    pub initial_distance: f32,
+    pub target_position: Vec2,
+}
 
 pub fn create_cannonball(commands: &mut Commands, world_position: Vec3, target_position: Vec2) -> Entity {
     let entity = commands.spawn(
@@ -31,7 +35,10 @@ pub fn create_cannonball(commands: &mut Commands, world_position: Vec3, target_p
     ).insert(
         (MarkerProjectile, MarkerCannonball)
     ).insert(
-        CannonballTarget(target_position)
+        CannonballTarget{
+            initial_distance: world_position.xy().distance(target_position),
+            target_position,
+        }
     ).id();
     entity
 }
@@ -41,8 +48,24 @@ pub fn cannonball_move_system(
     time: Res<Time>,
 ) {
     for (mut transform, target) in cannonballs.iter_mut() {
-        let direction_vector = (target.0.extend(0.) - transform.translation).normalize();
-        transform.translation += direction_vector * time.delta_seconds() * 300.;
+        let direction_vector = (target.target_position - transform.translation.xy()).normalize();
+        let move_distance = direction_vector * time.delta_seconds() * 200.;
+
+        let remaining_distance = (transform.translation.xy() + move_distance).distance(target.target_position);
+
+        // Calculate the progress as a value between 0 and 1
+        let progress = 1. - remaining_distance / target.initial_distance;
+
+        // Determine the scaling factor based on progress, applying a sine function for non-linearity
+        let scale_factor = if progress <= 0.5 {
+            1.0 + (PI * progress).sin()  // Non-linear scale up in the first half
+        } else {
+            (PI * (1.0 - progress)).sin() + 1.0  // Non-linear scale down in the second half
+        };
+        transform.scale = Vec3::splat(scale_factor);
+
+        // Move the cannonball
+        transform.translation += move_distance.extend(0.);
     }
 }
 
@@ -53,7 +76,7 @@ pub fn cannonball_hit_system(
     mut wisps: Query<&mut Health, With<Wisp>>,
 ) {
     for (entity, cannonball_transform, target) in cannonballs.iter() {
-        if cannonball_transform.translation.xy().distance(target.0) > 1. { continue; }
+        if cannonball_transform.translation.xy().distance(target.target_position) > 1. { continue; }
 
         let coords = GridCoords::from_transform(&cannonball_transform);
         for (dx, dy) in ALL_DIRECTIONS.iter().chain(&[(0, 0)]) {
